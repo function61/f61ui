1a2cf62c5634b85c1d25639654e9b1009aa1b71c
Joonas Loppi <joonas@joonas.fi>
2019-11-25 14:25:39 +0000

    progressbar: support colours

--- component/progressbar.tsx
+++ component/progressbar.tsx
@@ -1,17 +1,22 @@
 import * as React from 'react';
 
+type colourType = 'info' | 'success' | 'warning' | 'error';
+
 interface ProgressBarProps {
 	progress: number; // 0-100 [%]
+	colour?: colourType;
 }
 
 export class ProgressBar extends React.Component<ProgressBarProps, {}> {
 	render() {
 		const pct = Math.round(this.props.progress) + '%';
 
+		const colour: colourType = this.props.colour || 'info';
+
 		return (
 			<div className="progress" title={pct}>
 				<div
-					className="progress-bar progress-bar-info progress-bar-striped"
+					className={`progress-bar progress-bar-${colour} progress-bar-striped`}
 					style={{ width: pct }}
 				/>
 			</div>
