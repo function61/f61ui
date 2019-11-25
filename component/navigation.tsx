d0028e13157fff4de7324d068cff2fd28a2b6b30
Joonas Loppi <joonas@joonas.fi>
2019-11-25 14:24:52 +0000

    navigation: support glyphicons, export renderNavLink

--- component/navigation.tsx
+++ component/navigation.tsx
@@ -3,7 +3,23 @@ import * as React from 'react';
 export interface NavLink {
 	url: string;
 	title: string;
+	glyphicon?: string;
 	active: boolean;
+}
+
+export function renderNavLink(link: NavLink): React.ReactNode {
+	const activeOrNot = link.active ? 'active' : '';
+
+	return (
+		<li className={activeOrNot} key={link.url}>
+			<a href={link.url}>
+				{link.glyphicon && (
+					<span className={'glyphicon glyphicon-' + link.glyphicon}>&nbsp;</span>
+				)}
+				{link.title}
+			</a>
+		</li>
+	);
 }
 
 interface NavigationProps {
@@ -12,16 +28,6 @@ interface NavigationProps {
 
 export default class NavigationTabs extends React.Component<NavigationProps, {}> {
 	render() {
-		const items = this.props.links.map((link) => {
-			const activeOrNot = link.active ? 'active' : '';
-
-			return (
-				<li className={activeOrNot} key={link.url}>
-					<a href={link.url}>{link.title}</a>
-				</li>
-			);
-		});
-
-		return <ul className="nav nav-tabs">{items}</ul>;
+		return <ul className="nav nav-tabs">{this.props.links.map(renderNavLink)}</ul>;
 	}
 }
