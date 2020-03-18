cabcfdc13b65c6a87f88f6e0e93cd1e7c99514a6
Joonas Loppi <joonas@joonas.fi>
2020-03-18 10:53:21 +0200

    httputil: increase clarity by using async-await

--- httputil.ts
+++ httputil.ts
@@ -1,4 +1,6 @@
-export function getJson<T>(url: string): Promise<T> {
+import { StructuredErrorResponse } from 'f61ui/types';
+
+export async function getJson<T>(url: string): Promise<T> {
 	const headers: { [key: string]: string } = {
 		Accept: 'application/json',
 	};
@@ -8,16 +10,18 @@ export function getJson<T>(url: string): Promise<T> {
 		headers['x-csrf-token'] = csrfToken;
 	}
 
-	return fetch(url, { method: 'GET', headers })
-		.then(httpMustBeOk)
-		.then((response) => response.json());
+	const response = await fetch(url, { method: 'GET', headers });
+
+	await httpMustBeOk(response);
+
+	return await response.json();
 }
 
 export function postJson<I, O>(url: string, body: I): Promise<O> {
 	return postJsonReturningVoid<I>(url, body).then((res) => res.json());
 }
 
-export function postJsonReturningVoid<T>(url: string, body: T): Promise<Response> {
+export async function postJsonReturningVoid<T>(url: string, body: T): Promise<Response> {
 	const bodyToPost = JSON.stringify(body);
 
 	const headers: { [key: string]: string } = {
@@ -30,37 +34,38 @@ export function postJsonReturningVoid<T>(url: string, body: T): Promise<Response
 		headers['x-csrf-token'] = csrfToken;
 	}
 
-	return fetch(url, {
+	const response = await fetch(url, {
 		headers,
 		method: 'POST',
 		body: bodyToPost,
-	}).then(httpMustBeOk);
+	});
+
+	await httpMustBeOk(response);
+
+	return response;
 }
 
-export function httpMustBeOk(response: Response): Promise<Response> {
-	if (!response.ok) {
-		return new Promise<Response>((_: any, reject: any) => {
-			response.text().then(
-				(text: string) => {
-					if (response.headers.get('content-type') === 'application/json') {
-						reject(JSON.parse(text) as {});
-					} else {
-						reject(new Error('HTTP response failure: ' + text));
-					}
-				},
-				(err: Error) => {
-					reject(
-						new Error(
-							'HTTP response failure. Also, error fetching response body: ' +
-								err.toString(),
-						),
-					);
-				},
-			);
-		});
+export async function httpMustBeOk(response: Response): Promise<void> {
+	if (response.ok) {
+		return;
 	}
 
-	return Promise.resolve(response);
+	// response not ok, dig up response body to make a detailed error object
+	let responseBody: string;
+
+	try {
+		responseBody = await response.text();
+	} catch (err) {
+		throw new Error(
+			'HTTP response failure. Also, error fetching response body: ' + err.toString(),
+		);
+	}
+
+	if (response.headers.get('content-type') === 'application/json') {
+		throw JSON.parse(responseBody) as StructuredErrorResponse;
+	} else {
+		throw new Error('HTTP response failure: ' + responseBody);
+	}
 }
 
 export function makeQueryParams(path: string, queryParams: { [key: string]: string }): string {
