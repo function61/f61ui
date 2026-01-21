5fdde94a6291d21761cca22349402744e295c45b
Joonas Loppi <joonas+code@joonas.fi>
2026-01-21 13:27:53 +0200

    (WIP) try to revive dropdown

--- component/dropdown.tsx
+++ component/dropdown.tsx
@@ -1,3 +1,4 @@
+import { Dropdown as DropdownBase } from 'bootstrap';
 import * as React from 'react';
 
 interface DropdownProps {
@@ -6,28 +7,49 @@ interface DropdownProps {
 }
 
 export class Dropdown extends React.Component<DropdownProps, {}> {
+	private buttonRef = React.createRef<HTMLButtonElement>();
+
+	componentDidMount(): void {
+		if (this.buttonRef.current) {
+			alert('initing dropdownbase');
+			new DropdownBase(this.buttonRef.current);
+		}
+	}
+
 	render() {
-		const maybeLabel = this.props.label ? this.props.label + ' ' : '';
+		// const maybeLabel = this.props.label ? this.props.label + ' ' : '';
+		const maybeLabel = 'foo';
 
 		const children: React.ReactNode[] =
 			this.props.children instanceof Array ? this.props.children : [this.props.children];
 
 		const items = children.map((child, idx) => {
-			return <li key={idx}>{child}</li>;
+			return (
+				<li key={idx} className="dropdown-item">
+					{child}
+				</li>
+			);
 		});
 
 		return (
-			<div className="btn-group">
+			<div className="dropdown">
 				<button
 					type="button"
-					className="btn btn-default dropdown-toggle"
-					data-toggle="dropdown"
+					ref={this.buttonRef}
+					className="btn btn-secondary dropdown-toggle"
+					data-bs-toggle="dropdown"
 					aria-haspopup="true"
 					aria-expanded="false">
 					{maybeLabel}
-					<span className="caret" />
 				</button>
-				<ul className="dropdown-menu">{items}</ul>
+				<ul className="dropdown-menu">
+					<li>
+						<a className="dropdown-item" href="#">
+							Action
+						</a>
+					</li>
+					{items}
+				</ul>
 			</div>
 		);
 	}
